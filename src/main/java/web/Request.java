68a602da8cec25e00bfdef24a273dcbcbea86a35
JDrims <dimas.asus@mail.ru>
2023-11-01 04:36:51 +0600

    initional task 2

--- src/main/java/web/Request.java
+++ src/main/java/web/Request.java
@@ -1,15 +1,28 @@
 package web;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Request {
     private final String method;
     private final String path;
+    private final String body;
 
     public Request(String method, String path) {
         this.method = method;
         this.path = path;
+        this.body = null;
+    }
+
+    public Request(String method, String path, String body) {
+        this.method = method;
+        this.path = path;
+        this.body = body;
     }
 
     public String getMethod() {
@@ -23,5 +36,33 @@ public class Request {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String getQureyParam(String name) throws URISyntaxException {
+        List<NameValuePair> params = getQueryParams();
+        for (NameValuePair param : params) {
+            if (param.getName().equals(name)) {
+                return param.getValue();
+            }
+        }
+        return null;
+    }
+
+    public List<NameValuePair> getQueryParams() throws URISyntaxException {
+        return URLEncodedUtils.parse(new URI(this.path), StandardCharsets.UTF_8);
+    }
+
+    public String getPostParam(String name) {
+        List<NameValuePair> params = getPostParams();
+        for (NameValuePair param : params) {
+            if (param.getName().equals(name)) {
+                return param.getValue();
+            }
+        }
+        return null;
+    }
+
+    public List<NameValuePair> getPostParams() {
+        return URLEncodedUtils.parse(body, StandardCharsets.UTF_8);
     }
 }
--- src/main/java/web/Server.java
+++ src/main/java/web/Server.java
@@ -1,25 +1,19 @@
 package web;
 
-import org.apache.http.NameValuePair;
-
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
     private final List<String> validPaths = List.of("/index.html", "/spring.svg",
             "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html",
             "/forms.html", "/classic.html", "/events.html", "/events.js", "/default-get.html");
@@ -54,22 +48,76 @@ public class Server {
     }
 
     public void processing(Socket socket) {
-        try (final var out = new BufferedOutputStream(socket.getOutputStream());
-             final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+        final var allowedMethods = List.of(GET, POST);
+        try (
+                final var in = new BufferedInputStream(socket.getInputStream());
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            final var limit = 4096;
 
-            final var requestLine = in.readLine();
-            final var parts = requestLine.split(" ");
+            in.mark(limit);
+            final var buffer = new byte[limit];
+            final var read = in.read(buffer);
 
-            if (parts.length != 3) {
-                socket.close();
+            final var requestLineDelimiter = new byte[]{'\r', '\n'};
+            final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+            if (requestLineEnd == -1) {
+                responseWithoutContent(out, "404", "Not Found");
                 return;
             }
 
-            final var method = parts[0];
-            final var path = parts[1];
-            Request request = new Request(method, path);
+            final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+            if (requestLine.length != 3) {
+                responseWithoutContent(out, "404", "Not Found");
+                return;
+            }
 
-            if (request == null || !handlers.containsKey(request.getMethod())) {
+            final var method = requestLine[0];
+            if (!allowedMethods.contains(method)) {
+                responseWithoutContent(out, "404", "Not Found");
+                return;
+            }
+            System.out.println(method);
+
+            final var path = requestLine[1];
+            if (!path.startsWith("/")) {
+                responseWithoutContent(out, "404", "Not Found");
+                return;
+            }
+            System.out.println(path);
+
+            final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+            final var headersStart = requestLineEnd + requestLineDelimiter.length;
+            final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+            if (headersEnd == -1) {
+                responseWithoutContent(out, "404", "Not Found");
+                return;
+            }
+
+            in.reset();
+            in.skip(headersStart);
+
+            final var headersBytes = in.readNBytes(headersEnd - headersStart);
+            final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+            System.out.println(headers);
+
+            Request request = null;
+            if (!method.equals(GET)) {
+                in.skip(headersDelimiter.length);
+                final var contentLength = extractHeader(headers, "Content-Length");
+                if (contentLength.isPresent()) {
+                    final var length = Integer.parseInt(contentLength.get());
+                    final var bodyBytes = in.readNBytes(length);
+
+                    final var body = new String(bodyBytes);
+                    System.out.println(body);
+                    request = new Request(method, path, body);
+                }
+            } else {
+                request = new Request(method, path);
+            }
+
+            if (!handlers.containsKey(request.getMethod())) {
                 responseWithoutContent(out, "404", "Not Found");
                 return;
             }
@@ -148,5 +196,26 @@ public class Server {
             handlers.put(method, new HashMap<>());
         }
         handlers.get(method).put(path, handler);
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    private static Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
     }
 }
